1d57c76ca7dc73c92c99a27ecd5757f44f1084a1
Seu Nome <william.239993@fmm.org.bwilliamachline.com>
2025-09-13 12:40:48 -0400

    Primeiro commit

--- aula12.09/teste4.py
+++ aula12.09/teste4.py
@@ -1,14 +1,13 @@
 import cv2
 import numpy as np
-# Criar imagem em branco (500x500 pixels, 3 canais de cor)
-img = np.zeros((500, 500, 3), dtype="uint8")
-# Desenhar formas
-cv2.rectangle(img, (50,50), (200,200), (0,255,0), -1) # Retângulo verde
-cv2.circle(img, (300,300), 50, (255,0,0), -1) # Círculo azul
-cv2.line(img, (0,0), (500,500), (0,0,255), 5) # Linha vermelha
-# Texto
-cv2.putText(img, "Meu Nome", (150,250),
-cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+
+img = np.ones((500, 500, 3), dtype="uint8") * 255
+
+cv2.rectangle(img, (40, 40), (250, 250), (255, 165, 0), -1)
+cv2.circle(img, (350, 350), 75, (128, 0, 0), -1)
+cv2.line(img, (0, 500), (500, 0), (0, 128, 0), 5)
+cv2.putText(img, "William", (180, 260), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 0), 3)
+
 cv2.imshow("Desenho", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
